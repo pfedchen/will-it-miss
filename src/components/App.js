216347bf65bf5973836793a5b2c3faddc6ce9e4e
pfedchen <pfedchen@gmail.com>
2021-06-18 17:47:33 +0300

    Now some more Tzinovering

--- src/components/App.js
+++ src/components/App.js
@@ -41,7 +41,7 @@ export default function App() {
   return (
     <div>
       <p>
-        {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
+        {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be a Tzinovering {' '}
         <strong>{results.length}</strong> near misses
       </p>
       <hr></hr>
